ccb47c20ae04de1693ab49de6f633d135c2c32fc
Alexandr <alex.zavara1@gmail.com>
2020-12-27 23:36:54 +0300

    tests function

---
+++ test2/js/script.js
@@ -0,0 +1,14 @@
+var firstFunction = function () {     //Cоздает новую функцию, сохраняя ее в переменной "firstFunction"
+        console.log("Hello world!");
+};
+
+function sumSome (a, b) {
+    return a + b;
+}
+
+function multiplySome (a, b) {
+    return a * b;
+}
+
+
+//alert(sumSome(multiplySome(36325, 9824), 777));
