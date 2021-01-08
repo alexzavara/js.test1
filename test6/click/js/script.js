2b72969b2adbbdd01cf74b79afac72c959e71ad7
Alexandr <alex.zavara1@gmail.com>
2021-01-08 12:51:40 +0300

    jquery, click

---
+++ test6/click/js/script.js
@@ -0,0 +1,8 @@
+﻿
+
+$( "html" ).click(function(event){  //При нажатии на любую часть html 
+    $("#heading").offset({         //Найти id heading и передать координаты X и Y
+        left: event.pageX,
+        top: event.pageY
+    });
+});
