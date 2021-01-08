05c6a2b75fb5b4b17f236e95506a3c4e9f52c567
Alexandr <alex.zavara1@gmail.com>
2021-01-08 21:31:34 +0300

    test-animation

---
+++ test6/animation/js/script.js
@@ -0,0 +1,31 @@
+﻿
+var offsetTop = 0;
+var offsetLeft = 0;
+var heading = $("#heading").offset();
+
+
+
+
+var testAnimation = function() {
+
+    if (offsetTop === 0 && offsetLeft === 0){
+        offsetLeft = 200;
+    }
+    else if (offsetTop === 0 && offsetLeft === 200) {
+        offsetTop = 200;
+
+    }
+    else if (offsetTop === 200 && offsetLeft === 200) {
+        offsetLeft = 0;
+
+    }
+    else if (offsetTop === 200 && offsetLeft === 0) {
+        offsetTop = 0;
+        
+    }
+    $("#heading").offset({top:offsetTop, left:offsetLeft });
+};
+
+var timerId = setInterval(testAnimation, 500);
+
+
--- test6/click/js/script.js
+++ test6/click/js/script.js
@@ -1,5 +1,4 @@
 ﻿
-
 $( "html" ).click(function(event){  //При нажатии на любую часть html 
     $("#heading").offset({         //Найти id heading и передать координаты X и Y
         left: event.pageX,
